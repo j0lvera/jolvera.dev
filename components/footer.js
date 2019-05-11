d0732ae7b5129bb38c4547389a6623c3263ac426
j0lv3r4 <jolvera@posteo.de>
2019-05-10 21:40:59 -0500

    change source code link

--- components/footer.js
+++ components/footer.js
@@ -8,9 +8,7 @@ function Footer() {
           <p className="copy">
             &copy; {new Date().getFullYear()}, Proudly built with{" "}
             <a href="https://nextjs.org">Next.js</a> -{" "}
-            <a href="https://github.com/j0lv3r4/nextjs-blog-starter">
-              Source code
-            </a>
+            <a href="https://github.com/j0lv3r4/jolvera.dev">Source code</a>
           </p>
         </Container>
       </footer>

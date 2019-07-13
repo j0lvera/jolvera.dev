5b43acf47da2f0d00625fc8638272547b5c4fb41
j0lv3r4 <jolvera@posteo.de>
2019-07-12 19:37:14 -0500

    display commits in home page

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,35 @@
 import { jsx, css } from "@emotion/core";
 import { withTheme } from "emotion-theming";
 import { withRouter } from "next/router";
+import fetch from "isomorphic-unfetch";
 import { Link, Flex, Box, Text } from "@rebass/emotion";
 import { siteMeta } from "../blog.config";
 import Layout from "../components/layouts/default";
 
-const Home = ({ router, theme }) => {
+const Commit = ({ theme, commit }) => {
+  return (
+    <Box p={2} bg="rgba(0,0,0,0.1)" mb={3}>
+      <Link color={theme.color} mb={0} href={commit.url}>
+        commit {commit.sha}
+      </Link>
+
+      <Text as="p">
+        Author:{" "}
+        {commit.authorUrl ? (
+          <Link color={theme.color} href={commit.authorUrl}>
+            {commit.author}
+          </Link>
+        ) : (
+          <span>{commit.author}</span>
+        )}
+      </Text>
+
+      <Text as="p">{commit.message}</Text>
+    </Box>
+  );
+};
+
+const Home = ({ router, theme, commits }) => {
   return (
     <Layout pageTitle="Home" path={router.pathname}>
       <Flex
@@ -50,8 +74,54 @@ const Home = ({ router, theme }) => {
           </Text>
         </Box>
       </Flex>
+
+      <Box
+        css={css`
+          font-family: monospace;
+        `}
+      >
+        {commits.map((commit, index) => (
+          <Commit key={index} commit={commit} theme={theme} />
+        ))}
+      </Box>
     </Layout>
   );
+};
+
+Home.getInitialProps = async ctx => {
+  try {
+    const response = await fetch(
+      "https://api.github.com/repos/j0lv3r4/jolvera.dev/commits",
+      {
+        widthCredentials: true,
+        headers: {
+          Authentication: `Bearer: ${process.env.GITHUB_TOKEN}`
+        }
+      }
+    );
+
+    const arr = await response.json();
+    const commits = arr.map(({ commit, author, sha, html_url }) => {
+      return {
+        author: commit.author.name,
+        authorUrl: (author && author.html_url) || null,
+        url: html_url,
+        sha: sha,
+
+        message: commit.message
+      };
+    });
+
+    // console.log("arr", arr);
+    // console.log("commits", commits);
+
+    return {
+      commits
+    };
+  } catch (err) {
+    console.log("err", err);
+    return {};
+  }
 };
 
 export default withTheme(withRouter(Home));
